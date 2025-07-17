e9d7a37b268d198ac7155f02662baa069f3df899
Nikhil Sirsat <sirsatnikhil617@gmail.com>
2025-07-17 20:57:20 +0530

    Implement RATE LIMIT on askQuestion route using REDIS

---
+++ Backend/Utils/rateLimit.js
@@ -0,0 +1,32 @@
+import redis from '../config/redis.js';
+import ExpressError from './ExpressError.js';
+
+const DAILY_LIMIT = 50;
+
+export const rateLimiter = async (req, res, next) => {
+    try {
+        const userId = req.user._id;
+
+        const now = new Date();
+        const key = `rate:${userId}:${now.toISOString().split('T')[0]}`;
+
+        // Daily limit
+        const dailyCount = await redis.incr(key);
+        if (dailyCount === 1) {
+            await redis.expire(key, 24 * 60 * 60); // expire in 24 hours
+        }
+
+        if (dailyCount > DAILY_LIMIT) {
+            return res.status(429).json({ message: 'your daily limit exceeded. Please try again after 24 hours.' });
+        }
+
+        // Attach remaining limits to headers
+        res.set('X-RateLimit-Remaining-Day', Math.max(0, DAILY_LIMIT - dailyCount));
+
+        next();
+
+    } catch (err) {
+        console.error('Rate Limiting Error:', err);
+        throw new ExpressError(500, 'Rate limiting error occurred');
+    }
+};
--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -3,8 +3,9 @@ const router = express.Router();
 import { auth, isAuther } from '../Middleware/AuthMid.js';
 import { askQuestion, getMessages, getConversations, deleteConversation } from '../controllers/chat.js';
 import { asyncHandler } from '../Middleware/asyncHandler.js';
+import { rateLimiter } from '../Utils/rateLimit.js';
 
-router.post('/', auth, asyncHandler(askQuestion));
+router.post('/', auth, rateLimiter, asyncHandler(askQuestion));
 
 router.get('/:conversationId/messages', auth, asyncHandler(getMessages));
 
