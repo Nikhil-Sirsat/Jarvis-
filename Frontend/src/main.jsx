95c079fd3485af752a387c07dfe1259e44fdde68
Nikhil Sirsat <sirsatnikhil617@gmail.com>
2025-07-23 15:44:03 +0530

    resolved authcontext import issue in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App.jsx';
 
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './Context/AuthContext.jsx';
 import { SnackbarProvider } from './Context/SnackBarContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
