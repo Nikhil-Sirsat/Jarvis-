bc4a04232603af025b7c05a714e3fca05fe4fa82
Nikhil Sirsat <sirsatnikhil617@gmail.com>
2025-08-01 22:50:10 +0530

    changed sentence transformer model and include normalization for faster vector generation

--- embedding_service/embedding_service.py
+++ embedding_service/embedding_service.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
+from contextlib import asynccontextmanager
 
-# Load model once at startup
-model = SentenceTransformer("intfloat/e5-small-v2")  # Fast & high-quality
-
-app = FastAPI()
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 class EmbedRequest(BaseModel):
     text: str
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model.encode("warmup", normalize_embeddings=True)
+    print("Quantized model warmed up!")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 @app.post("/embed")
 def embed_text(req: EmbedRequest):
-    vector = model.encode(req.text).tolist()
+    vector = model.encode(req.text, normalize_embeddings=True, convert_to_numpy=False).tolist()
     return {"embedding": vector}
